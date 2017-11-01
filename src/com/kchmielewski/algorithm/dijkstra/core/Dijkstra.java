cfc0a1051940ca1644a42fde36304d32e40b42e8
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-01 21:03:28 +0100

    Task 4: defined proper method to be implemented

---
+++ src/com/kchmielewski/algorithm/dijkstra/core/Dijkstra.java
@@ -0,0 +1,11 @@
+package com.kchmielewski.algorithm.dijkstra.core;
+
+import com.kchmielewski.algorithm.dijkstra.structure.Graph;
+import com.kchmielewski.algorithm.dijkstra.structure.Vertex;
+
+public class Dijkstra {
+    //FIXME: implementation
+    public <T> float calculate(Graph<T> graph, Vertex<T> from, Vertex<T> to) {
+        return 0f;
+    }
+}
