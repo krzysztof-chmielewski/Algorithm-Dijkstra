37239fc8b2b2dd6ec9aca4277a54733a8a5d54b8
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-05 20:14:03 +0100

    Task 14: implementation of Dijkstra Algorithm now also tracks road for each vertex

--- src/com/kchmielewski/algorithm/dijkstra/core/Dijkstra.java
+++ src/com/kchmielewski/algorithm/dijkstra/core/Dijkstra.java
@@ -5,14 +5,20 @@ import com.kchmielewski.algorithm.dijkstra.structure.Graph;
 import com.kchmielewski.algorithm.dijkstra.structure.Vertex;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Dijkstra {
-    public <T> Optional<Float> calculate(Graph<T> graph, Vertex<T> from, Vertex<T> to) {
+    public <T> Optional<Result> calculate(Graph<T> graph, Vertex<T> from, Vertex<T> to) {
         Set<Vertex<T>> vertices = new HashSet<>(graph.vertices());
         vertices.stream().filter(v -> v != from).forEach(v -> v.value(Float.MAX_VALUE));
         from.value(0);
 
         Queue<Vertex<T>> queue = new PriorityQueue<>(vertices);
+        Map<Vertex<T>, List<Vertex<T>>> roads = vertices.stream().collect(Collectors.toMap(v -> v, v -> {
+            List<Vertex<T>> list = new ArrayList<>();
+            list.add(v);
+            return list;
+        }));
 
         while (!queue.isEmpty()) {
             Vertex<T> current = queue.remove();
@@ -20,6 +26,9 @@ public class Dijkstra {
                 Vertex<T> neighbour = edge.other(current);
                 float newValue = current.value() + edge.value();
                 if (newValue < neighbour.value()) {
+                    List<Vertex<T>> newRoad = new ArrayList<>(roads.get(current));
+                    newRoad.add(neighbour);
+                    roads.put(neighbour, newRoad);
                     neighbour.value(newValue);
                     queue.remove(neighbour);
                     queue.add(neighbour);
@@ -31,6 +40,32 @@ public class Dijkstra {
             return Optional.empty();
         }
 
-        return Optional.of(to.value());
+        return Optional.of(new Result<>(to.value(), roads.get(to)));
+    }
+
+    public static class Result<T> {
+        private final float distance;
+        private final List<Vertex<T>> road;
+
+        private Result(float distance, List<Vertex<T>> road) {
+            this.distance = distance;
+            this.road = road;
+        }
+
+        public float distance() {
+            return distance;
+        }
+
+        public List<Vertex<T>> road() {
+            return road;
+        }
+
+        @Override
+        public String toString() {
+            return "Result{" +
+                    "distance=" + distance +
+                    ", road=" + road +
+                    '}';
+        }
     }
 }
