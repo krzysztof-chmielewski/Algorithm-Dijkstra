21008d52cf0be3f9ed6f9880cc539ce88511e611
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-05 20:03:11 +0100

    Task 13: retrofitted way the cities was read from file to more be intuitive and concise.

--- src/com/kchmielewski/algorithm/dijkstra/example/Main.java
+++ src/com/kchmielewski/algorithm/dijkstra/example/Main.java
@@ -11,7 +11,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "resources/cities.txt";
+        Map<String, Vertex<String>> cities = readCitiesFromFile("resources/cities.txt");
+        Graph<String> graph = new Graph<>(new ArrayList<>(cities.values()));
+        Dijkstra dijkstra = new Dijkstra();
+
+        System.out.println("Distance from Krakow to Gdansk is: ");
+        System.out.println(dijkstra.calculate(graph, cities.get("Krakow"), cities.get("Gdansk")));
+        System.out.println("Distance from Gdansk to Krakow is: ");
+        System.out.println(dijkstra.calculate(graph, cities.get("Gdansk"), cities.get("Krakow")));
+        System.out.println("Distance from New York to Boston is: ");
+        System.out.println(dijkstra.calculate(graph, cities.get("New York"), cities.get("Boston")));
+        System.out.println("Distance from New York to Poznan is: ");
+        System.out.println(dijkstra.calculate(graph, cities.get("New York"), cities.get("Poznan")));
+    }
+
+    private static Map<String, Vertex<String>> readCitiesFromFile(String fileName) {
         List<String> lines = null;
         try {
             lines = Files.readAllLines(Paths.get(fileName));
@@ -20,39 +34,25 @@ public class Main {
             e.printStackTrace();
             System.exit(0);
         }
-        Map<Vertex<String>, Vertex<String>> cities = new HashMap<>();
+        Map<String, Vertex<String>> cities = new HashMap<>();
 
         lines.forEach(l -> {
-            String[] line = l.split(";");
-            Vertex<String> firstCity = new Vertex<>(line[0]);
-            Vertex<String> secondCity = new Vertex<>(line[1]);
-            float distance = Float.parseFloat(line[2]);
-            if (!cities.containsKey(firstCity)) {
-                cities.put(firstCity, firstCity);
+            Scanner scanner = new Scanner(l);
+            scanner.useDelimiter(";");
+            String firstCityName = scanner.next();
+            String secondCityName = scanner.next();
+            float distance = scanner.nextFloat();
+            Vertex<String> firstCity = new Vertex<>(firstCityName);
+            Vertex<String> secondCity = new Vertex<>(secondCityName);
+            if (!cities.containsKey(firstCityName)) {
+                cities.put(firstCityName, firstCity);
             }
-            if (!cities.containsKey(secondCity)) {
-                cities.put(secondCity, secondCity);
+            if (!cities.containsKey(secondCityName)) {
+                cities.put(secondCityName, secondCity);
             }
-            cities.get(firstCity).link(cities.get(secondCity), distance);
+            cities.get(firstCityName).link(cities.get(secondCityName), distance);
         });
-        Graph<String> graph = new Graph<>(new ArrayList<>(cities.values()));
-        Dijkstra dijkstra = new Dijkstra();
 
-        System.out.println("Distance from Krakow to Gdansk is: ");
-        System.out.println(dijkstra.calculate(graph,
-                cities.get(new Vertex<>("Krakow")),
-                cities.get(new Vertex<>("Gdansk"))));
-        System.out.println("Distance from Gdansk to Krakow is: ");
-        System.out.println(dijkstra.calculate(graph,
-                cities.get(new Vertex<>("Gdansk")),
-                cities.get(new Vertex<>("Krakow"))));
-        System.out.println("Distance from New York to Boston is: ");
-        System.out.println(dijkstra.calculate(graph,
-                cities.get(new Vertex<>("New York")),
-                cities.get(new Vertex<>("Boston"))));
-        System.out.println("Distance from New York to Poznan is: ");
-        System.out.println(dijkstra.calculate(graph,
-                cities.get(new Vertex<>("New York")),
-                cities.get(new Vertex<>("Poznan"))));
+        return cities;
     }
 }
